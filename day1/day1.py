8660004161e62a5af1aec7d195492591820e3046
Jatin Suteri <jatinsuteri@gmail.com>
2024-12-07 02:06:19 +0530

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -16,3 +16,15 @@ while i < len(lis1):
     diff += abs(lis1[i] - lis2[i])
     i += 1
 print(diff)
+
+##part 2
+
+from collections import Counter
+counter_lis2 = Counter(lis2)
+similarity = 0
+
+for i in lis1:
+    if i in counter_lis2:
+        if counter_lis2[i]:
+            similarity += counter_lis2[i] * i
+print(similarity)
